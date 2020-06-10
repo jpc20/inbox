64944c6e761ad1b51344de1b142c08df3d91f0b7
Jack Cullen <jpcullen20@gmail.com>
2020-06-10 15:05:57 -0600

    Finish lesson

---
+++ deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'sister race cabin monitor slender orchard fork budget brass polar best ripple',
+  'https://rinkeby.infura.io/v3/1385039fb2fb405a9272dec867afb43b'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: '0x' + bytecode, arguments: ['hello world'] })
+    .send({ from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+};
+deploy();
+
+/* https://rinkeby.infura.io/v3/1385039fb2fb405a9272dec867afb43b */
